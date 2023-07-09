4ac5c0759392f222beb27af4ab717979ed43aa34
miraclemichael018 <miraclemichael018@gmail.com>
2023-07-09 21:15:37 +0000

    some changes

--- components/AlertMsg.vue
+++ components/AlertMsg.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<div
-			class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 w-[300px] rounded absolute right-0 z-50 top-[10px]"
+			class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 w-[300px] rounded absolute right-[20px] z-50 top-[10px]"
 			role="alert"
 		>
 			<!-- <strong class="font-bold">Holy smokes!</strong> -->
@@ -9,7 +9,7 @@
 				<slot></slot>
 			</span>
 			<span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-				<svg
+				<!-- <svg
 					class="fill-current h-6 w-6 text-red-500"
 					role="button"
 					xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@
 					<path
 						d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
 					/>
-				</svg>
+				</svg> -->
 			</span>
 		</div>
 	</div>
